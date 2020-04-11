3448a74b3636fac0d1e92c836b23a4a6618ec982
vermadev54 <vermadev54@gmail.com>
2020-04-11 20:52:08 +0530

    First commit

--- src/config.py
+++ src/config.py
@@ -1,13 +1,14 @@
 import transformers
 
 
-MAX_LEN = 512
+MAX_LEN = 256
 TRAIN_BATCH_SIZE = 8
-VALID_BATCH_SIZE = 4
+VALID_BATCH_SIZE = 8
 EPOCHS = 10
-BERT_PATH = "../input/bert_base_uncased/"
+BERT_PATH = "../input/bert-base-uncased/"
 MODEL_PATH = "model.bin"
-TRAINING_FILE = "../input/imdb.csv"
+#TRAINING_FILE = "../input/imdb.csv"
+TRAINING_FILE = "../tweet_input/tweet_input.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(
     BERT_PATH,
     do_lower_case=True
--- src/train.py
+++ src/train.py
@@ -15,7 +15,8 @@ from transformers import get_linear_schedule_with_warmup
 def run():
     dfx = pd.read_csv(config.TRAINING_FILE).fillna("none")
     dfx.sentiment = dfx.sentiment.apply(
-        lambda x: 1 if x == "positive" else 0
+        #lambda x: 1 if x == "positive" else 0
+        lambda x: 1 if x == "1" else 0
     )
 
     df_train, df_valid = model_selection.train_test_split(
@@ -55,11 +56,13 @@ def run():
     model.to(device)
     
     param_optimizer = list(model.named_parameters())
+    #print( param_optimizer)
     no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
     optimizer_parameters = [
         {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.001},
         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0},
     ]
+    #print(optimizer_parameters)
 
     num_train_steps = int(len(df_train) / config.TRAIN_BATCH_SIZE * config.EPOCHS)
     optimizer = AdamW(optimizer_parameters, lr=3e-5)
